41644d13be36211b4175d5900e03b1b16b78db35
KajaSk <kskucha@student.agh.edu.pl>
2026-06-11 07:25:39 +0200

    poprawa działania

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,7 @@ import {defineConfig} from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
     base: '/zadanie-5-urodziny/',
-})
-plugins: [tailwindcss(), ] 
+    plugins: [
+        tailwindcss(), 
+    ],
+});
